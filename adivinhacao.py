215993b28fb6c85dd8d46ce789d4c1ba1a680c78
Gabriel Souza <104119228+Gabrielworkdev@users.noreply.github.com>
2023-11-25 08:46:12 -0300

    comentário

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,6 +8,8 @@ print("")
 print("")
 print("")
 
+
+#declarando o número secreto
 numero_secreto = 42
 
 #interação com o usuário
@@ -15,8 +17,9 @@ chute_str = input("digite o seu número:  \n")
 
 #incluindo um texto 
 print("Você digitou", chute_str)
+#declarando que a palavra chute irá receber uma string
 chute = int(chute_str)
-#comparando o valor digitado com o nº secreto
+#comparando o valor digitado com o nº secreto "=="
 if (numero_secreto == chute):
 
     print("você acertou!")
