ecbd7fb527651ec885ceb56fec174775dff79752
Gabriel Souza <104119228+Gabrielworkdev@users.noreply.github.com>
2023-11-25 10:04:29 -0300

    while
    inserindo laço de repetição

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,29 +1,48 @@
-print("*********************************")
-print("")
-print("")
-print("")
-print ("JOGO DE ADIVINHAÇÃO")
-print("*********************************")
-print("")
-print("")
-print("")
+print("*********************************\n")
+print ("JOGO DE ADIVINHAÇÃO \n\n")
+print("********************************* \n")
+
 
 
 #declarando o número secreto
 numero_secreto = 42
 
-#interação com o usuário
-chute_str = input("digite o seu número:  \n")
+totalTentativas = 3
+rodada = 1
 
-#incluindo um texto 
-print("Você digitou", chute_str)
-#declarando que a palavra chute irá receber uma string
-chute = int(chute_str)
-#comparando o valor digitado com o nº secreto "=="
-if (numero_secreto == chute):
+while( rodada <= totalTentativas ):
+    print("Tentativas", rodada, "de", totalTentativas)
 
-    print("você acertou!")
-else:
-    print("Você errou!")
+    #interação com o usuário
+    chute_str = input("digite o seu número:  \n")
 
-print("Fim do Jogo!")
+    #incluindo um texto 
+    print("Você digitou", chute_str)
+    #declarando que a palavra chute irá receber uma string
+    #convertendo
+    chute = int(chute_str)
+    #comparando o valor digitado com o nº secreto "=="
+
+    if (numero_secreto == chute):
+
+        print("você acertou!")
+
+
+    #adicionando novas oportunidas ao usuário
+
+
+
+    else :
+        if(chute > numero_secreto):
+
+            print("VocÊ errou ! O seu chute foi maior do que o número secreto ")
+
+        elif (chute < numero_secreto):
+
+            print("VocÊ errou ! O seu chute foi menor do que o número secreto ")
+
+    rodada = rodada + 1
+    print("")
+    print("")
+
+    print("Fim do Jogo!")
