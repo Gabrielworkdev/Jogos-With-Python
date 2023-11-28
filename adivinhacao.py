cce76a45350206137e17ca3d50a019a58600dacf
Gabriel Souza <104119228+Gabrielworkdev@users.noreply.github.com>
2023-11-28 14:48:00 -0300

    new

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,50 +1,40 @@
 print("*********************************\n")
 print ("JOGO DE ADIVINHAÇÃO \n\n")
 print("********************************* \n")
-
-
-
 #declarando o número secreto
 numero_secreto = 42
-
 totalTentativas = 3
 rodada = 1
 #funcao .formart {rodada} de {totalTentaivas} baseada na string 
+for rodada in range(1, totalTentativas +1 ):
 
-#operador menor ou igual
-while( rodada <= totalTentativas ):
-    print("Tentativas {} de {}".format( rodada, totalTentativas))
+    print("Tentativas {} de {}".format(rodada, totalTentativas))
 
     #interação com o usuário
-    chute_str = input("digite o seu número:  \n")
+    chute_str = input("digite o seu número de 1 a 100:  \n")
 
     #incluindo um texto 
     print("Você digitou", chute_str)
+
     #declarando que a palavra chute irá receber uma string
     #convertendo
     chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior = chute < numero_secreto
+    menor = chute > numero_secreto
+
+    
+    
     #comparando o valor digitado com o nº secreto "=="
-
-    if (numero_secreto == chute):
-
+    if (acertou):
         print("você acertou!")
-
-
-    #adicionando novas tentativas ao usuário
-
-
-
+        break
+   #adicionando novas tentativas ao usuário
     else :
-        if(chute > numero_secreto):
-
+        if(maior):
             print("VocÊ errou ! O seu chute foi maior do que o número secreto ")
-
-        elif (chute < numero_secreto):
-
+        elif (menor):
             print("VocÊ errou ! O seu chute foi menor do que o número secreto ")
-
-    rodada = rodada + 1
     print("")
     print("")
-
-    print("Fim do Jogo!")
+print("Fim do Jogo!")
