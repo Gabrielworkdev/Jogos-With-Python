b29429ecf7a3df2b522f082b1755462dd226ecb2
Gabriel Souza <104119228+Gabrielworkdev@users.noreply.github.com>
2023-11-30 15:40:52 -0300

    new

--- jogos.py
+++ jogos.py
@@ -1,7 +1,19 @@
+#importando outros jogos 
+import forca
+import adivinhacao
+
 print("*********************************\n")
 print ("**** ESCOLHA SEU JOGO **** \n")
 print("********************************* \n")
 
+print("(1) Forca (2) adivinhação")
+jogo = int(input("Qual jogo?")) #capturando o input #funcao biult
+
+
+if (jogo == 1):
+    print("Você escolheu: ")
+elif(jogo ==2):
+    print("jogando adivinhação")
 
 
 print("Fim do Jogo!")
