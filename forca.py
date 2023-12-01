5ab66e1df532c966ea52b816167ce1f899eb2ee3
Gabriel Souza <104119228+Gabrielworkdev@users.noreply.github.com>
2023-12-01 09:43:56 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -3,8 +3,14 @@ def jogarForca():
     print ("*** BEM VINDO AO JOGO DE FORCA ***\n")
     print("********************************* \n")
 
+    palavraSecreta = "banana"
 
+    enforcou = False # variavel booleana - RETORNA FALSO OU VERDADEIRA 
+    acertou = False
 
+    #enquanto n se enforcouo E n acertou - segue o jogo
+    while(not enforcou and not acertou): #palavras chave no python sempre se usa em letras minúscula
+        print("Jogando ..")
 
     print("Fim do Jogo!")
 if(__name__ == "__main__"): #verificando se a variavel possui valor especial e só executa quando selecionamos a mesma.
