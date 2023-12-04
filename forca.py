141d4914d4cb7afa340808553ad57e460efa6752
Gabriel Souza <104119228+Gabrielworkdev@users.noreply.github.com>
2023-12-04 19:53:17 -0300

    new

--- forca.py
+++ forca.py
@@ -5,6 +5,7 @@ def jogarForca():
     print("********************************* \n")
 
     palavra_secreta = "banana"
+    letra_acertada = ["_","_","_","_","_","_"]
 
     enforcou = False # variavel booleana - RETORNA FALSO OU VERDADEIRA 
     acertou = False
@@ -12,16 +13,18 @@ def jogarForca():
     while(not enforcou and not acertou): #palavras chave no python sempre se usa em letras minúscula
         
         chute = input("Qual Letra?")
+        chute = chute.strip()#usado para retirar os espaços no inicio e final
         # for verificque a letra dentro de palavra !
 
         index = 0 #posição de cada letra.
         for letra in palavra_secreta:
-            if (chute == letra):
-                 print("Encontrei a letra  na posição ")#.format(letra, index)) #interpolação de string
-            index = index +1 #incrimentando a dados  a cada letra 
+            if (chute.upper() == letra.upper()):#coloca o texto em maiúsculo
+                 letra_acertada[index] = letra #guardando as letras corretas inseridas pela usuario
+            index = index + 1 #incrimentando a dados  a cada letra 
+        print(letra_acertada)
 
     print("Jogando...")
              
-    print("Fim do Jogo!")
+print("Fim do Jogo!")
 if(__name__ == "__main__"): #verificando se a variavel possui valor especial e só executa quando selecionamos a mesma.
     jogarForca()
